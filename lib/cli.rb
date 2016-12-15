f22b280c1ac04e34a9c5e9c4649c9e5a9a856573
brownielox <mrsmpython@gmail.com>
2016-12-14 20:48:44 -0500

    cli update 2

--- lib/cli.rb
+++ lib/cli.rb
@@ -15,3 +15,30 @@ class CommandLineInteface
    page_arr = Scraper.new.scrape_webpage(BASE_PATH)
 
 end
+
+  def self.scrape_webpage(index_url)
+
+    doc = Nokogiri::HTML(open(index_url))
+
+    location_array = []
+
+    doc.css("").each do |locale|
+
+      location_array << {
+
+          name: locale.css(".odd:nth-child(0)").text,
+
+          address: locale.css(".odd:nth-child(1)").text,
+
+          day: locale.css(".odd:nth-child(2)").text,
+
+          hours: locale.css(".odd:nth-child(3)").text,
+
+          borough: locale.css(".odd:nth-child(4)").text,
+
+        }
+      end
+    location_array
+    end
+
+puts page_arr
