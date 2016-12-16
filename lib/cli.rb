a4d2560a1e9676af4dbf65b303a021c8ec981360
brownielox <mrsmpython@gmail.com>
2016-12-15 21:29:52 -0500

    cli changes

--- lib/cli.rb
+++ lib/cli.rb
@@ -8,40 +8,33 @@ class CommandLineInteface
   BASE_PATH = "https://www.grownyc.org/compost/locations"
 
 
-  def self.scrape_webpage(index_url)
-
-    doc = Nokogiri::HTML(open("https://www.grownyc.org/compost/locations"))
-
-    location_array = []
-
-    doc.css("").each do |locale|
-
-      location_array << {
-
-          name: locale.css(".odd:nth-child(0)").text,
-
-          address: locale.css(".odd:nth-child(1)").text,
-
-          day: locale.css(".odd:nth-child(2)").text,
-
-          hours: locale.css(".odd:nth-child(3)").text,
-
-          borough: locale.css(".odd:nth-child(4)").text,
-
-        }
-      end
-    location_array
-  end
-
-  def chat
-
    puts "Where would you like to compost?"
    gets.strip.chomp
 
-   page_arr = self.new.scrape_webpage(BASE_PATH)
+   # page_arr = self.new.scrape_webpage(BASE_PATH)
 
-  end
   # puts page_arr
-end
+ 
 
-chat
+    doc = Nokogiri::HTML(open(BASE_PATH))
+
+    location_hash = []
+
+    doc.css(".odd").each do |locale|
+
+      location_hash << {
+
+          name: locale.css(".odd:nth-child(0)").text,
+
+          # address: locale.css(".odd:nth-child(1)").text,
+
+          # day: locale.css(".odd:nth-child(2)").text,
+
+          # hours: locale.css(".odd:nth-child(3)").text,
+
+          # borough: locale.css(".odd:nth-child(4)").text,
+
+        }
+      end
+    puts location_array[1]
+end
